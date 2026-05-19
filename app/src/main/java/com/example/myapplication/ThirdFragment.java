561a8720bee295ebb689b24b51c6e6fdae0342a9
kris510-a11y <kristallsajfer510@gmail.com>
2026-05-19 23:34:40 +0600

    Добавлены SecondActivity и ThirdActivity с Intent-флагами

--- app/src/main/java/com/example/myapplication/ThirdFragment.java
+++ app/src/main/java/com/example/myapplication/ThirdFragment.java
@@ -7,24 +7,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import androidx.fragment.app.Fragment;
 
-public class SecondFragment extends Fragment {
+public class ThirdFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_third, container, false);
 
         Button buttonBack = view.findViewById(R.id.buttonBack);
         buttonBack.setOnClickListener(v -> getParentFragmentManager().popBackStack());
-
-        Button buttonOpenThird = view.findViewById(R.id.buttonOpenThird);
-        buttonOpenThird.setOnClickListener(v -> {
-            ThirdFragment thirdFragment = new ThirdFragment();
-            getParentFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, thirdFragment)
-                    .addToBackStack(null)
-                    .commit();
-        });
 
         return view;
     }
